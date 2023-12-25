01f70897844065493627948f3dfee00f05c96917
KhailoAndrey <hap512@gmail.com>
2023-12-25 16:41:01 +0200

    rafactoring front

--- dd/src/App.js
+++ dd/src/App.js
@@ -1,47 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 import AuthPage from './AuthPage';
 import RegistrationPage from './RegistrationPage';
-import ViemAllPage from "./ViemAllPage";
+import ViewAllPage from './ViewAllPage';
+import './App.css'
+import './styles/divWithImage.css'
 
 const App = () => {
     return (
         <Router>
-            <div>
-                <h1>Team Final Project </h1>
-                <div className="divWithImage">
-                    <img src="/images/page1.jpg" alt="Start page"/>
-                </div>
-                <div className="divWithImage">
-                    <img src="/images/page2.jpg" alt="Foto team"/>
-                </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <button>
-                                <Link to="/login">Login</Link>
-                            </button>
-                        </li>
-                        <li>
-                            <button>
-                                <Link to="/register">Register</Link>
-                            </button>
-                        </li>
-                        <li>
-                            <button>
-                                <Link to="/viemAll">Viem All</Link>
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
+            <Routes>
+                <Route path='/' index element={<Home />} />
+                <Route path="/login" element={<AuthPage />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/viewAll" element={<ViewAllPage />} />
+            </Routes>
 
-                <Routes>
-                    <Route path="/login" element={<AuthPage/>}/>
-                    <Route path="/register" element={<RegistrationPage/>}/>
-                    <Route path="/viemAll" element={<ViemAllPage/>}/>
-                </Routes>
-
-            </div>
         </Router>
     );
 };
