dcb9b12fe10eb2b368a62e7c1316d8ef211e80be
KhailoAndrey <hap512@gmail.com>
2024-01-02 23:11:49 +0200

    add conditional render

--- dd/src/ViewAllPage.js
+++ dd/src/ViewAllPage.js
@@ -7,6 +7,8 @@ import dataArr from './test.json'
 const ViewAllPage = () => {
     const [data, setData] = useState([]);
     const [error, setError] = useState('');
+    const [selectedOption, setSelectedOption] = useState(null);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -42,27 +44,114 @@ const ViewAllPage = () => {
 
         fetchData();
     }, []);
+
+    const handleConvert = async () => { 
+        // тут должна быть логика обработки конвертации ссылок 
+    }
+
+    const handleRadioChange = (value) => {
+        setSelectedOption(value);
+        // сюда при смене радио кнопки надо повесить обработку фильтра с бека 
+        // запросом или как то еще
+    };
+
     console.log('data', data)
+    const isAuthToken = localStorage.getItem('authToken')
     return (
         <div className='App-container'>
             <h1 className='Auth-title'>View All</h1>
             {error && <p>{error}</p>}
-
-            <div className='Info-title Form-label'>
-                <p>OriginalUrl</p>
-                <p>ShortUrl</p>
-            </div>
-            {data.map((item) => (
-                <div key={item.id} className='Info-all'>
-                    <div className='info-text'>
-                    <span><a href={item.originalUrl}>{item.originalUrl}</a></span>
+            {isAuthToken ?
+                <>
+                    <div className='Convert-block'>
+                        <form className='Form'>
+                            <label className='Form-label'>
+                                <p>
+                                    Original Url:
+                                </p>
+                                <input
+                                    type="text"
+                                    value={''}
+                                />
+                            </label>
+                            <br />
+                            <label className='Form-label'>
+                                <p>
+                                    Description:
+                                </p>
+                                <input
+                                    type="password"
+                                    value={''}
+                                />
+                            </label>
+                            <br />
+                        </form>
+                        <button type="button" onClick={handleConvert} className='button'>
+                            Convert
+                        </button>
                     </div>
-                    <div className='info-text'>
-                    <p><a href={item.shortUrl}>{item.shortUrl}</a></p>
-                    <p className='visit-count'><BsEye /> {item.visitCount}</p>
+                    <div className='radio-block'>
+                        <label className="radio-label">
+                            <input
+                                type="radio"
+                                name="radioGroup"
+                                value="option1"
+                                checked={selectedOption === 'option1'}
+                                onChange={() => handleRadioChange('option1')}
+                            />
+                            user urls
+                        </label>
+                        <label className="radio-label">
+                            <input
+                                type="radio"
+                                name="radioGroup"
+                                value="option2"
+                                checked={selectedOption === 'option2'}
+                                onChange={() => handleRadioChange('option2')}
+                            />
+                            active urls
+                        </label>
                     </div>
-                </div>
-            ))}
+                    <>
+                    <div className='Info-title Form-label'>
+                        <p>ShortUrl</p>
+                        <p>OriginalUrl</p>
+                    </div>
+                    {data.map((item) => (
+                        <div key={item.id} className='Info-all'>
+                            <div className='info-text'>
+                                <p><a href={item.shortUrl}>{item.shortUrl}</a></p>
+                                <p><BsEye /> {item.visitCount}</p>
+                            </div>
+                            <div className='info-text'>
+                                <span><a href={item.originalUrl}>{item.originalUrl}</a></span>
+                            </div>
+                            <button type='button' className='button'>edit</button>
+                            {/* сюда кнопкам надо певесить обработчик по onClick */}
+                            <button type='button' className='button'>delete</button>
+                        </div>
+                    ))}
+                </>
+                </>
+                :
+                <>
+                    <div className='Info-title Form-label'>
+                        <p>ShortUrl</p>
+                        <p>OriginalUrl</p>
+                    </div>
+                    {data.map((item) => (
+                        <div key={item.id} className='Info-all'>
+                            <div className='info-text'>
+                                <p><a href={item.shortUrl}>{item.shortUrl}</a></p>
+                                <p><BsEye /> {item.visitCount}</p>
+                            </div>
+                            <div className='info-text'>
+                                <span><a href={item.originalUrl}>{item.originalUrl}</a></span>
+                            </div>
+                        </div>
+                    ))}
+                </>
+            }
         </div>
     );
 };
